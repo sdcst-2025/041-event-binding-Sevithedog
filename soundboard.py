2d4db65a03d18bc4e5541f98e58660df81a8b6b2
Finlay <finlay.mitchner@gmail.com>
2025-04-22 11:35:25 -0700

    x

--- soundboard.py
+++ soundboard.py
@@ -2,18 +2,33 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 import playsound as p 
+from PIL import Image, ImageTk
+
+croc = Image.open("Bombardirocroc.png")
+bomb = croc.resize((50,50))
+tral = Image.open("Tralalero.png")
+tralalero = tral.resize((50,50))
 
 def playbomb(event):
     print(event)
-    p.playsound("Bombardiro.jpg")
+    p.playsound("Voicy_Bombardino Crocodilo.mp3")
+def playtral(event):
+    print(event)
+    p.playsound("tralalero-tralala-Meme-Sound.mp3")
 
 window = tk.Tk()
 #Labels
-phbomb = tk.PhotoImage(file = "Bombardiro_Crocodilo.webp")
-bombardirolb = tk.Label(window, image = phbomb)
+bomba = ImageTk.PhotoImage(bomb)
+shark = ImageTk.PhotoImage(tralalero)
+#bombardirolb = tk.Label(window, image = phbomb)
 
 
 #Buttons
-bombardirobt = tk.Button()
-
+bombardirobt = tk.Button(window,image = bomba )
+bombardirobt.bind("<Button>", playbomb)
+tralbt = tk.Button(window, image = shark)
+tralbt.bind("<Button>", playtral)
 #Positioning
+bombardirobt.place(x=0,y=50,width = 50, height = 50)
+tralbt.place(x=50, y = 50)
+window.mainloop() 
